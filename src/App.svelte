82baae134bdfbce5ce1874b8089c4a91751d7ff8
Erikote <erikote2004@icloud.com>
2023-11-10 11:25:42 +0100

    Reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
 <script>
-    function manageClick() {
-        console.log("You're brave");
+    let name = "";
+    function handle(e) {
+        const target = e.target;
+        name = e.target.value;
     }
 </script>
 
-<button on:click={manageClick}>
-    Click me if you dare
-</button>
+<input 
+    class="text" 
+    type="text" 
+    placeholder="Name" 
+    on:keyup={handle} 
+/>
+
+<p>Hello, {name}. Your name has {name.length} characters</p>
+
+<style>
+    .text {
+        font-size: 1.5rem;
+    }
+</style>
