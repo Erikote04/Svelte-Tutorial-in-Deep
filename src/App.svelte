ba20809768c4894d00f7ab179fd0f49390591559
Erikote <erikote2004@icloud.com>
2023-11-10 11:03:30 +0100

    Capturing events

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,9 @@
 <script>
-    import Inner from "./Nested.svelte";
-    export let adjective;
-    let word = "World";
+    function manageClick() {
+        console.log("You're brave");
+    }
 </script>
 
-<h1>Hello {word}!!!</h1>
-<p>Hope you're having a {adjective} day</p>
-<Inner adjective="great" />
-
-<style>
-    h1 {
-        font-size: 3rem;
-        text-align: center;
-        color: red;
-    }
-
-    p {
-        font-size: 1.5rem;
-        text-align: center;
-        color: blue;
-    }
-</style>
+<button on:click={manageClick}>
+    Click me if you dare
+</button>
