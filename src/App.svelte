85a554184f634523145f74597714113d50934cf6
user-daw-zayas <erikote2004@icloud.com>
2023-11-18 20:16:01 +0100

    Each block statement. Child components

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,17 @@
 <script>
+    import Task from "./Task.svelte"
+
     let tasks = [
-        "Cut the grass",
-        "Wash the dishes",
-        "Take out the trash"
+        { name: "Cut the grass", finished: true },
+        { name: "Wash the dishes", finished: false },
+        { name: "Take out the trash", finished: true }
     ];
 </script>
 
 <h3>Tasks:</h3>
 
-{#each tasks as task, index}
-    <p><strong>{index}:</strong> {task}</p>
+{#each tasks as task}
+    <Task name={task.name} finished={task.finished} />
 {/each}
 
 
---
+++ src/Task.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let name;
+    export let finished;
+</script>
+
+{#if finished}
+    <p><del>{name}</del> ✅</p>
+{:else}
+    <p>{name}</p>
+{/if}
