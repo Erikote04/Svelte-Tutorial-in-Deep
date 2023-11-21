ee3174d17849046cd16f84f93c0b42e3a77ff27d
Erikote <erikote2004@icloud.com>
2023-11-21 09:47:19 +0100

    Binding variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,6 @@
 <script>
-// Simulates a network request
-    function fetch() {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve({
-                    name: "Erik S. de Erice",
-                    age: 19,
-                    occupation: "Web Developer"
-                })
-                reject({
-                    code: 503,
-                    message: "We are having problems connecting to the network"
-                })
-            }, 3000)
-        })
-    }
+    let name = "Erik"
 </script>
 
-{#await fetch()} <!--Waiting (3 seconds)-->
-    <p>One second, we're resolving things...</p>
-{:then playload} <!--resolve()-->
-    <p>Nombre: {playload.name}</p>
-    <p>Age: {playload.age}</p>
-    <p>Occupation: {playload.occupation}</p>
-{:catch e} <!--reject(). Comment the resolve block to see this message-->
-    <p>Error <strong>{e.code}</strong>: {e.message}</p>
-{/await}
+<p>Your name is {name}</p>
+<p><input type="text" bind:value={name}></p>
