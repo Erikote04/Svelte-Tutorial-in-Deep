8f78bde0a488d4911a0d5fce708ba988d1c775ec
Erikote <erikote2004@icloud.com>
2023-12-02 21:33:36 +0100

    Full example of events. Select component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
     import Input from "./lib/Input.svelte";
+    import Select from "./lib/Select.svelte";
     
     let userData = {
         name: "Erik",
@@ -10,6 +11,8 @@
             max: 21000
         }
     }
+
+    let fields = ['Backend', 'Frontend', 'Devops', 'QA'];
 
     function send() {
         alert(JSON.stringify(userData))
@@ -27,6 +30,12 @@
             identifier = "firstName"
             formField = "First name"
             bind:value = {userData.firstName}
+        />
+        <Select 
+            identifier = "field"
+            formField = "Field"
+            choices = {fields}
+            bind:value = {userData.field}
         />
         <p>
             <input type="submit" value="Send">
---
+++ src/lib/Select.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let identifier;
+    export let formField;
+    export let choices;
+    export let value;
+</script>
+
+<p>
+    <label for={identifier}>{formField}: </label>
+    <select id={identifier} bind:value>
+        {#each choices as choice}
+            <option value={choice}>{choice}</option>
+        {/each}
+    </select>
+</p>
