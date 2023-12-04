04b01cc9d92a939a80323048755105ce370aded4
Erikote <erikote2004@icloud.com>
2023-12-04 19:36:50 +0100

    Working with CSS. Conditional classes (3)

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -2,16 +2,28 @@
     export let label;
     export let value;
 
+    let valid = value.length > 0;
+    let invalid = value.length === 0;
     let id = "_" + label.toLowerCase();
+
+    function update() {
+        valid = value.length > 0;
+        invalid = value.length === 0;
+    }
 </script>
 
-<p class:invalid={value.length === 0}>
+<p class:valid class:invalid>
     <label class="label" for={id}>{label}: </label>
     <input 
         type = "text" 
         id = {id} 
         bind:value
-    />
+        on:keyup={update} 
+    /> 
+    <!--
+        on:keyup is used because Svelte does not always update the value of the variable.
+        Later we'll see another way to fix this problem. 
+    -->
 </p>
 
 <style>
