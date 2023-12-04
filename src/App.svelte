6edc7544402b9fd87bfdec2c6cc2c406a0064c99
Erikote <erikote2004@icloud.com>
2023-12-04 20:48:43 +0100

    Working with CSS. Import images using paths in variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
     import "./assets/global.css";
     import "normalize.css/normalize.css";
+    import Svelte from "./assets/svelte.svg";
     import Input from "./lib/Input.svelte";
 
     let name = "John";
@@ -10,8 +11,10 @@
 <main>
     <Input label="Name" bind:value={name} />
     <Input label="Firstname" bind:value={firstName} />
-
     <p class="label">Your name is {name} {firstName}</p>
+
+    <p>{Svelte}</p> <!--The variable contains the path-->
+    <img src={Svelte} alt="Svelte logo">
 </main>
 
 <style>
