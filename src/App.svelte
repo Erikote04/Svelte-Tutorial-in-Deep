b942f58b490df6b254405ab49646b0ad7e3bdbbb
Erikote <erikote2004@icloud.com>
2023-12-05 13:37:30 +0100

    Multiple slots. Trick: svelte:fragment does not add new tags to the DOM (div tag was added to the DOM in the previous commit)

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,10 @@
 
 <main>
     <Section title="Menu of the Day">
-        <div slot="main">
+        <svelte:fragment slot="main">
             <p>Taco Tuesday</p>
             <p>Margaritas</p>
             <p>Nachos with cheese</p>
-        </div>
+        </svelte:fragment>
     </Section>
 </main>
