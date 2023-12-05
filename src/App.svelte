71b21dbc0888598282ad414670b7378f54ad8616
Erikote <erikote2004@icloud.com>
2023-12-05 13:31:28 +0100

    Multiple slots. Error: duplicate slot name

--- src/App.svelte
+++ src/App.svelte
@@ -5,5 +5,7 @@
 <main>
     <Section title="Menu of the Day">
         <p slot="main">Taco Tuesday</p>
+        <p slot="main">Margaritas</p>
+        <p slot="main">Nachos with cheese</p>
     </Section>
 </main>
